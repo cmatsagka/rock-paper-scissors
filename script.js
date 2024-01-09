e57c0bae3bc55e1bddde26541c5595dd85683e49
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-09 20:47:34 +0200

    Code single round of game in script.js

--- script.js
+++ script.js
@@ -2,16 +2,36 @@ function getComputerChoice() {
     let computermove = Math.floor(Math.random() * 3);
 
     if (computermove === 0){
-        console.log("Rock");
-        return "Rock";
+        return "rock";
         } else if (computermove === 1) {
-            console.log("Paper");
-        return "Paper";
+        return "paper";
     } else {
-        console.log("Scissors");
-        return "Scissors";
+        return "scissors";
     }
 }
 
-getComputerChoice();
+const playerSelection = prompt("Rock, Paper or Scissors?", "").toLowerCase();
+const computerSelection = getComputerChoice();
 
+function playRound(playerSelection, computerSelection){
+
+    if (playerSelection == computerSelection) {
+        return "It's a tie! Rematch!";
+    } else {
+        if (playerSelection == "rock"  && computerSelection == "paper") {
+            return "You lose! Paper beats Rock";
+        } else if (playerSelection == "rock" && computerSelection == "scissors"){
+            return "You win! Rock beats Scissors";
+        } else if (playerSelection == "paper" && computerSelection == "rock") {
+            return "You win! Paper beats Rock";
+        } else if (playerSelection == "paper" && computerSelection == "scissors") {
+            return "You lose! Scissors beats Paper";
+        } else if (playerSelection == "scissors" && computerSelection == "rock") {
+            return "You lose! Rock beats Scissors";
+        } else if (playerSelection == "scissors" && computerSelection == "paper") {
+            return "You win! Scissors beats Paper";
+        }
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
