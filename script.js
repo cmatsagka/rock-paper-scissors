a3009e89de44b7d23d66c9b55252c5737155f5f9
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2025-11-28 14:56:03 +0200

    Write computerChoice code

---
+++ script.js
@@ -0,0 +1,16 @@
+function computerChoice(){
+    let rock = 0;
+    let paper = 1;
+    let scissors = 2;
+    let choice;
+    choice = Math.floor(Math.random() * 3);
+
+    if (choice === rock) {
+        return 'rock';
+    }else if (choice === paper) {
+        return 'paper';
+    }else{
+        return 'scissors';
+    }
+
+}
