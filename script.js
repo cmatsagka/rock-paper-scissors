f1e4ea61012555eef982c8b6b43970fcfc260c8d
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2025-12-07 20:32:51 +0200

    Add button for each option and eventListeners for each

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt('Rock? Paper? or Scissors?');
-    console.log('human = ', choice);
-    return choice.toLowerCase();
-}
+// function getHumanChoice(btn) {
+//     let choice = btn.id;
+//     console.log('human = ', choice);
+//     return choice.toLowerCase();
+// }
 
 function playRound(humanChoice, computerChoice) {
 
@@ -49,12 +49,12 @@ function playGame() {
     let round = 1;
     let winner = '';
 
-    while (round <= 5) {
-        console.log('Round ', round);
-        playRound(getHumanChoice(), getComputerChoice());
-        console.log('Score is: Player ' + humanScore + ' - Computer: ' + computerScore);
-        round++;
-    }
+    // while (round <= 5) {
+    //     console.log('Round ', round);
+    //     playRound(getHumanChoice(), getComputerChoice());
+    //     console.log('Score is: Player ' + humanScore + ' - Computer: ' + computerScore);
+    //     round++;
+    // }
 
     if (humanScore === computerScore){
         return 'It was a tie! Final score is: Player ' + humanScore + ' - Computer ' + computerScore;
@@ -66,3 +66,16 @@ function playGame() {
 
     return 'Winner is ' + winner + '! Final Score is: Player ' + humanScore + ' - Computer ' + computerScore;  
 }
+
+const buttons = document.querySelectorAll('button');
+let computerChoice = getComputerChoice();
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let humanChoice = button.id;
+        console.log('Human: ' + humanChoice);
+        playRound(humanChoice, computerChoice);
+    });
+});
+
+playGame();
